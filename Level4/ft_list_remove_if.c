78e34d20492eb1ef45b4faa1fffcb1aff79f510a
Maria João Balouta <165146704+mjbalouta@users.noreply.github.com>
2025-06-04 14:18:19 +0100

    Update ft_list_remove_if.c

--- Level4/ft_list_remove_if.c
+++ Level4/ft_list_remove_if.c
@@ -1,23 +1,26 @@
 #include "ft_list.h"
 
-void ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
+void	ft_list_remove_if(t_list **begin_list, void *data_ref, int (*cmp)())
 {
-	t_list *to_free;
 	t_list *current;
+	t_list *to_free;
 
+	if (!begin_list || !*begin_list)
+		return ;
 	current = *begin_list;
-	while (current->next)
+	while (current && current->next)
 	{
-		if ((*cmp)(current->next->data, data_ref) == 0)
+		if (((*cmp)(data_ref, current->next->data) == 0))
 		{
 			to_free = current->next;
 			current->next = current->next->next;
 			free(to_free);
 		}
-		current = current->next;
+		else
+			current = current->next;
 	}
 	current = *begin_list;
-	if (current && ((*cmp)(current->data, data_ref == 0))
+	if (current && ((*cmp)(data_ref, current->data) == 0))
 	{
 		*begin_list = current->next;
 		free(current);
